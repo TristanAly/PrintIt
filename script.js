7a0b3c1c04c5a91903bc981add03b0709246c2a9
TristanAly <tristan.aly@gmail.com>
2023-08-02 17:55:01 +0200

    dot's background color change with slide

--- script.js
+++ script.js
@@ -27,15 +27,23 @@ const textSlideSelected = document.querySelector(".text-slide");
 let numero = 0;
 let next = 1;
 let précedent = -1;
+let dot = 0;
 
 function slideLeft() {
     numero = numero + précedent;
+    dot = dot + précedent;
   
     if (numero < 0) 
     numero = slides.length - 1;
     dot = numero;
     imageSelected.src = `./assets/images/slideshow/${slides[numero].image}`;
     textSlideSelected.innerHTML = slides[numero].tagLine;
+
+    allDot[dot].classList.add("dot_selected");
+  let dottest = dot + 1;
+  if (dot < allDot.length - 1) allDot[dottest].classList.remove("dot_selected");
+  else if (allDot[0].classList.contains("dot_selected"))
+    allDot[0].classList.remove("dot_selected");
 
     console.log("nextSlideLeft");
 }
@@ -49,6 +57,24 @@ function slideRight() {
     dot = numero;
     imageSelected.src = `./assets/images/slideshow/${slides[numero].image}`;
     textSlideSelected.innerHTML = slides[numero].tagLine;
+
+    // j'ajoute la classe "dot_selected" a mon element
+  allDot[dot].classList.add("dot_selected");
+  // je décrémente dot pour permettre d'enlever la classe "dot_selected" sur la précedente
+  let dottest = dot - 1 ;
+  // je fais une condition qui dit que tant que la variable dottest et inferieur au tableau et inferieur ou égal a 0. il m'enleve les classe des précedentes.
+  // si ce n'est pas le cas tu me met la variable dottest egale au dernier élement du tableau pour qu'on puisse remove la classe "dot_selected" du dernier élément
+  console.log(dottest);
+  if ((dottest < allDot.length - 1) && (dottest >= 0)){ 
+  console.log(dottest);
+  allDot[dottest].classList.remove("dot_selected");
+  console.log(numero);
+  }
+  else {
+    dottest = allDot.length - 1
+    allDot[dottest].classList.remove("dot_selected");
+    console.log(dottest);
+  }
 
     console.log("nextSlideRight");
 }
